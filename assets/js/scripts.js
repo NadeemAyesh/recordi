39b77c0c28613ce14813a9bafd0f3bc5b4bc2c7c
n.ayesh <n.ayesh@newline.ps>
2020-07-11 20:22:49 +0300

    upload final with responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     //data-tab="#profile"
     $('.modal-toggle').on('click', function (e) {
         var tab = e.currentTarget.hash;
-        console.log(tab)
+        console.log(tab);
         // $('.nav-tabs li > a[href="' + tab + '"]').tab("show");
 
         $('.login-register').on('show.bs.modal', function (e) {
@@ -55,7 +55,7 @@ $(document).ready(function () {
 
     $('.reactions-single a').on('click', function (e) {
         e.preventDefault();
-        $(this).addClass('active')
+        $(this).addClass('active');
     });
 
     // Comments Add comment
@@ -187,115 +187,37 @@ function responseMessage(msg) {
     $('.success-box div.text-message').html("<span>" + msg + "</span>");
 }
 
-/* ---- particles.js config ---- */
+// upload file
 
-// particlesJS("particles-js", {
-//     particles: {
-//         number: {
-//             value: 80,
-//             density: {
-//                 enable: true,
-//                 value_area: 800
-//             }
-//         },
-//         color: {
-//             value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"]
-//         },
-//         shape: {
-//             type: ["circle"],
-//             stroke: {
-//                 width: 0,
-//                 color: "#fff"
-//             },
-//             polygon: {
-//                 nb_sides: 5
-//             },
-//             image: {
-//                 src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
-//                 width: 100,
-//                 height: 100
-//             }
-//         },
-//         opacity: {
-//             value: 1,
-//             random: false,
-//             anim: {
-//                 enable: false,
-//                 speed: 1,
-//                 opacity_min: 0.1,
-//                 sync: false
-//             }
-//         },
-//         size: {
-//             value: 8,
-//             random: true,
-//             anim: {
-//                 enable: false,
-//                 speed: 10,
-//                 size_min: 10,
-//                 sync: false
-//             }
-//         },
-//         line_linked: {
-//             enable: true,
-//             distance: 150,
-//             color: "#808080",
-//             opacity: 0.4,
-//             width: 1
-//         },
-//         move: {
-//             enable: true,
-//             speed: 5,
-//             direction: "none",
-//             random: false,
-//             straight: false,
-//             out_mode: "out",
-//             bounce: false,
-//             attract: {
-//                 enable: false,
-//                 rotateX: 600,
-//                 rotateY: 1200
-//             }
-//         }
-//     },
-//     interactivity: {
-//         detect_on: "canvas",
-//         events: {
-//             onhover: {
-//                 enable: true,
-//                 mode: "grab"
-//             },
-//             onclick: {
-//                 enable: true,
-//                 mode: "push"
-//             },
-//             resize: true
-//         },
-//         modes: {
-//             grab: {
-//                 distance: 140,
-//                 line_linked: {
-//                     opacity: 1
-//                 }
-//             },
-//             bubble: {
-//                 distance: 400,
-//                 size: 40,
-//                 duration: 2,
-//                 opacity: 8,
-//                 speed: 3
-//             },
-//             repulse: {
-//                 distance: 200,
-//                 duration: 0.4
-//             },
-//             push: {
-//                 particles_nb: 4
-//             },
-//             remove: {
-//                 particles_nb: 2
-//             }
-//         }
-//     },
-//     retina_detect: true
-// });
+function readURL(input) {
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
